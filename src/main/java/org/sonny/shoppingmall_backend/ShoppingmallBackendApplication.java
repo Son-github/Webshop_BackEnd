e2a963911c13af2922b74f4bbcee8013f0a76540
son <jongin718@gmail.com>
2024-12-31 00:22:28 +0900

    Initial Commit

---
+++ src/main/java/org/sonny/shoppingmall_backend/ShoppingmallBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sonny.shoppingmall_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sonny/shoppingmall_backend/ShoppingmallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonny.shoppingmall_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
